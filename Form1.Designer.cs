ec0402ce95f7668ea56329e0cc99ec3bc451cc1f
david alejandro izquierdo cafaro <davidbeisbol@hotmail.com.ar>
2023-04-09 18:10:46 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Fechas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.lblDiaDeHoy = new System.Windows.Forms.Label();
+            this.txtDia = new System.Windows.Forms.TextBox();
+            this.lblDia = new System.Windows.Forms.Label();
+            this.lblMes = new System.Windows.Forms.Label();
+            this.lblAño = new System.Windows.Forms.Label();
+            this.lblDiaDeMañ = new System.Windows.Forms.Label();
+            this.lblRespuesta = new System.Windows.Forms.Label();
+            this.txtMes = new System.Windows.Forms.TextBox();
+            this.txtAnual = new System.Windows.Forms.TextBox();
+            this.epvTextos = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.epvTextos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIngresar.Location = new System.Drawing.Point(12, 115);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(75, 23);
+            this.btnIngresar.TabIndex = 0;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // lblDiaDeHoy
+            // 
+            this.lblDiaDeHoy.AutoSize = true;
+            this.lblDiaDeHoy.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiaDeHoy.Location = new System.Drawing.Point(12, 9);
+            this.lblDiaDeHoy.Name = "lblDiaDeHoy";
+            this.lblDiaDeHoy.Size = new System.Drawing.Size(63, 13);
+            this.lblDiaDeHoy.TabIndex = 1;
+            this.lblDiaDeHoy.Text = "Si hoy es:";
+            // 
+            // txtDia
+            // 
+            this.txtDia.Location = new System.Drawing.Point(12, 73);
+            this.txtDia.Name = "txtDia";
+            this.txtDia.Size = new System.Drawing.Size(35, 20);
+            this.txtDia.TabIndex = 2;
+            this.txtDia.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDia_KeyPress);
+            // 
+            // lblDia
+            // 
+            this.lblDia.AutoSize = true;
+            this.lblDia.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDia.Location = new System.Drawing.Point(12, 47);
+            this.lblDia.Name = "lblDia";
+            this.lblDia.Size = new System.Drawing.Size(28, 13);
+            this.lblDia.TabIndex = 3;
+            this.lblDia.Text = "Día";
+            // 
+            // lblMes
+            // 
+            this.lblMes.AutoSize = true;
+            this.lblMes.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMes.Location = new System.Drawing.Point(75, 47);
+            this.lblMes.Name = "lblMes";
+            this.lblMes.Size = new System.Drawing.Size(30, 13);
+            this.lblMes.TabIndex = 4;
+            this.lblMes.Text = "Mes";
+            // 
+            // lblAño
+            // 
+            this.lblAño.AutoSize = true;
+            this.lblAño.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAño.Location = new System.Drawing.Point(145, 47);
+            this.lblAño.Name = "lblAño";
+            this.lblAño.Size = new System.Drawing.Size(29, 13);
+            this.lblAño.TabIndex = 5;
+            this.lblAño.Text = "Año";
+            // 
+            // lblDiaDeMañ
+            // 
+            this.lblDiaDeMañ.AutoSize = true;
+            this.lblDiaDeMañ.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiaDeMañ.Location = new System.Drawing.Point(271, 9);
+            this.lblDiaDeMañ.Name = "lblDiaDeMañ";
+            this.lblDiaDeMañ.Size = new System.Drawing.Size(84, 13);
+            this.lblDiaDeMañ.TabIndex = 6;
+            this.lblDiaDeMañ.Text = "Mañana será:";
+            // 
+            // lblRespuesta
+            // 
+            this.lblRespuesta.AutoSize = true;
+            this.lblRespuesta.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRespuesta.Location = new System.Drawing.Point(271, 80);
+            this.lblRespuesta.Name = "lblRespuesta";
+            this.lblRespuesta.Size = new System.Drawing.Size(0, 17);
+            this.lblRespuesta.TabIndex = 7;
+            // 
+            // txtMes
+            // 
+            this.txtMes.Location = new System.Drawing.Point(78, 73);
+            this.txtMes.Name = "txtMes";
+            this.txtMes.Size = new System.Drawing.Size(35, 20);
+            this.txtMes.TabIndex = 8;
+            this.txtMes.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMes_KeyPress);
+            // 
+            // txtAnual
+            // 
+            this.txtAnual.Location = new System.Drawing.Point(148, 73);
+            this.txtAnual.Name = "txtAnual";
+            this.txtAnual.Size = new System.Drawing.Size(52, 20);
+            this.txtAnual.TabIndex = 9;
+            this.txtAnual.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAnual_KeyPress);
+            // 
+            // epvTextos
+            // 
+            this.epvTextos.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
+            this.epvTextos.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 173);
+            this.Controls.Add(this.txtAnual);
+            this.Controls.Add(this.txtMes);
+            this.Controls.Add(this.lblRespuesta);
+            this.Controls.Add(this.lblDiaDeMañ);
+            this.Controls.Add(this.lblAño);
+            this.Controls.Add(this.lblMes);
+            this.Controls.Add(this.lblDia);
+            this.Controls.Add(this.txtDia);
+            this.Controls.Add(this.lblDiaDeHoy);
+            this.Controls.Add(this.btnIngresar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.epvTextos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Label lblDiaDeHoy;
+        private System.Windows.Forms.TextBox txtDia;
+        private System.Windows.Forms.Label lblDia;
+        private System.Windows.Forms.Label lblMes;
+        private System.Windows.Forms.Label lblAño;
+        private System.Windows.Forms.Label lblDiaDeMañ;
+        private System.Windows.Forms.Label lblRespuesta;
+        private System.Windows.Forms.TextBox txtMes;
+        private System.Windows.Forms.TextBox txtAnual;
+        private System.Windows.Forms.ErrorProvider epvTextos;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fechas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+
+            //limpieza del error provider
+            epvTextos.Clear();
+
+            //validacion para llenar los campos
+            if (ValidarLlenado())
+            {
+
+            }
+            else
+            {
+                //valores traidos de los textboxs
+                int dia = Convert.ToInt32(txtDia.Text);
+                int mes = Convert.ToInt32(txtMes.Text);
+                int anual = Convert.ToInt32(txtAnual.Text);
+                
+                if (ValidarDatos())
+                {
+
+                }
+                else
+                {
+
+                    //Variable de año bisiesto
+                    bool anualTipo = false;
+                    if (anual%4 == 0)
+                    {
+                        anualTipo = true;
+                    }
+
+
+                    switch (mes)
+                    {
+                        case 1:
+                            if (dia==31)
+                            {
+                                lblRespuesta.Text = "1 de Febrero de " + anual;
+                                break;
+                            }
+                            else
+                            {
+                                lblRespuesta.Text = dia+1 + " de Enero de " + anual;
+                                break;
+                            }
+                        case 2:
+                            if (dia == 31 || dia ==30 )
+                            {
+                                epvTextos.SetError(txtDia, "Febrero no tiene " + dia + " dias");
+                                break;
+                            }
+                            else if (anualTipo && dia == 29)
+                            {
+                                lblRespuesta.Text = "1 de Marzo de " + anual;
+                                break;
+                            }
+                            else if (!anualTipo && dia == 29)
+                            {
+                                epvTextos.SetError(txtDia, "Febrero este año no tiene 29 dias");
+                                break;
+                            }
+                            else if (!anualTipo && dia == 28)
+                            {
+                                lblRespuesta.Text = "1 de Marzo de " + anual;
+                                break;
+                            }
+                            else if (anualTipo && dia == 28)
+                            {
+                                lblRespuesta.Text = "29 de Febrero de " + anual;
+                                break;
+                            }
+                            else
+                            {
+                                lblRespuesta.Text = dia + 1 + " de Febrero de " + anual;
+                                break;
+                            }
+                        case 3:
+                            if (dia == 31)
+                            {
+                                lblRespuesta.Text = "1 de Abril de " + anual;
+                                break;
+                            }
+                            else
+                            {
+                                lblRespuesta.Text = dia + 1 + " de Marzo de " + anual;
+                                break;
+                            }
+
+                        case 4:
+                            if (dia == 31)
+                            {
+                                epvTextos.SetError(txtDia, "Abril tiene 30 dias");
+                                break;
+                            }
+                            else if (dia == 30)
+                            {
+                                lblRespuesta.Text = "1 de Mayo de " + anual;
+                                break;
+                            }
+                            else
+                            {
+                                lblRespuesta.Text = dia + 1 + " de Abril de " + anual;
+                                break;
+                            }
+                        case 5:
+                            if (dia == 31)
+                            {
+                                lblRespuesta.Text = "1 de Junio de " + anual;
+                                break;
+                            }
+                            else
+                            {
+                                lblRespuesta.Text = dia + 1 + " de Mayo de " + anual;
+                                break;
+                            }
+                        case 6:
+                            if (dia == 31)
+                            {
+                                epvTextos.SetError(txtDia, "Junio tiene 30 dias");
+                                break;
+                            }
+                            else if (dia == 30)
+                            {
+                                lblRespuesta.Text = "1 de Julio de " + anual;
+                                break;
+                            }
+                            else
+                            {
+                                lblRespuesta.Text = dia + 1 + " de Junio de " + anual;
+                                break;
+                            }
+                        case 7:
+                            if (dia == 31)
+                            {
+                                lblRespuesta.Text = "1 de Agosto de " + anual;
+                                break;
+                            }
+                            else
+                            {
+                                lblRespuesta.Text = dia + 1 + " de Julio de " + anual;
+                                break;
+                            }
+                        case 8:
+                            if (dia == 31)
+                            {
+                                lblRespuesta.Text = "1 de Septiembre de " + anual;
+                                break;
+                            }
+                            else
+                            {
+                                lblRespuesta.Text = dia + 1 + " de Agosto de " + anual;
+                                break;
+                            }
+                        case 9:
+                            if (dia == 31)
+                            {
+                                epvTextos.SetError(txtDia, "Septiembre tiene 30 dias");
+                                break;
+                            }
+                            else if (dia == 30)
+                            {
+                                lblRespuesta.Text = "1 de Octubre de " + anual;
+                                break;
+                            }
+                            else
+                            {
+                                lblRespuesta.Text = dia + 1 + " de Septiembre de " + anual;
+                                break;
+                            }
+                        case 10:
+                            if (dia == 31)
+                            {
+                                lblRespuesta.Text = "1 de Noviembre de " + anual;
+                                break;
+                            }
+                            else
+                            {
+                                lblRespuesta.Text = dia + 1 + " de Octubre de " + anual;
+                                break;
+                            }
+                        case 11:
+                            if (dia == 31)
+                            {
+                                epvTextos.SetError(txtDia, "Noviembre tiene 30 dias");
+                                break;
+                            }
+                            else if (dia == 30)
+                            {
+                                lblRespuesta.Text = "1 de Diciembre de " + anual;
+                                break;
+                            }
+                            else
+                            {
+                                lblRespuesta.Text = dia + 1 + " de Noviembre de " + anual;
+                                break;
+                            }
+                        case 12:
+                            if (dia == 31)
+                            {
+                                lblRespuesta.Text = "1 de Enero de " + (anual+1);
+                                break;
+                            }
+                            else
+                            {
+                                lblRespuesta.Text = dia + 1 + " de Diciembre de " + anual;
+                                break;
+                            }
+                    }
+                }
+            }
+        }
+
+        //metodo para tener que llenar los campos
+        private bool ValidarLlenado()
+        {
+            bool validar = false;
+
+            if (txtDia.Text == "")
+            {
+                epvTextos.SetError(txtDia, "llenar campo");
+                validar = true;
+            }
+            if (txtMes.Text == "")
+            {
+                epvTextos.SetError(txtMes, "llenar campo");
+                validar = true;
+            }
+            if (txtAnual.Text == "")
+            {
+                epvTextos.SetError(txtAnual, "llenar campo");
+                validar = true;
+            }
+            return validar;
+        }
+
+        private bool ValidarDatos()
+        {
+            bool validar = false;
+
+            if (Convert.ToInt32(txtDia.Text) < 1 || Convert.ToInt32(txtDia.Text) > 31)
+            {
+                epvTextos.SetError(txtDia, "Los días van de 1 a 31");
+                validar = true;
+            }
+            if (Convert.ToInt32(txtMes.Text) < 1 || Convert.ToInt32(txtMes.Text) > 12)
+            {
+                epvTextos.SetError(txtMes, "Los meses van de 1 a 12");
+                validar = true;
+            }
+            return validar;
+        }
+
+        //Validacion para solo cargar numeros 
+        private void txtDia_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((!char.IsNumber(e.KeyChar)) && (!char.IsControl(e.KeyChar)))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtMes_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((!char.IsNumber(e.KeyChar)) && (!char.IsControl(e.KeyChar)))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtAnual_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((!char.IsNumber(e.KeyChar)) && (!char.IsControl(e.KeyChar)))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
